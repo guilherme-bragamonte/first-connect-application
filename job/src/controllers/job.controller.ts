27eb415d754ca6579a98b5d0c8ebb96e6ab90475
Guilherme <guilherme.bragamonte@objectedge.com>
2023-08-09 17:51:49 -0300

    add: job app base inserting a log into a custom object

--- job/src/controllers/job.controller.ts
+++ job/src/controllers/job.controller.ts
@@ -3,6 +3,7 @@ import { Request, Response } from 'express';
 import CustomError from '../errors/custom.error';
 import { logger } from '../utils/logger.utils';
 import { allOrders } from '../orders/fetch.orders';
+import { createApiRoot } from '../client/create.client';
 
 /**
  * Exposed job endpoint.
@@ -16,6 +17,19 @@ export const post = async (_request: Request, response: Response) => {
     // Get the orders
     const limitedOrdersObject = await allOrders({ sort: ['lastModifiedAt'] });
     logger.info(`There are ${limitedOrdersObject.total} orders!`);
+
+    const customObject = await createApiRoot()
+    .customObjects()
+    .post({
+      body: {
+        container: 'test-connect-container',
+        key: 'test-job-key',
+        value: `There are ${limitedOrdersObject.total} orders!`
+      }
+    })
+    .execute();
+
+    logger.info(JSON.stringify({ customObject }));
 
     response.status(200).send();
   } catch (error) {
